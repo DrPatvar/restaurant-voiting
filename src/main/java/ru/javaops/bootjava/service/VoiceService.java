cb6da96dfabff46915487f03469847f93409145f
DrPatvar <Shurkoff@gmail.com>
2023-05-13 19:48:35 +0300

    VoiceService

---
+++ src/main/java/ru/javaops/bootjava/service/VoiceService.java
@@ -0,0 +1,30 @@
+package ru.javaops.bootjava.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.javaops.bootjava.model.Voice;
+import ru.javaops.bootjava.repository.VoiceRepository;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class VoiceService {
+
+    @Autowired
+    protected VoiceRepository voiceRepository;
+
+    public List<Voice> getAll(){
+        return voiceRepository.findAll();
+    }
+
+    public void update(Voice voice){
+        LocalDateTime localDateTimeDB = voiceRepository.get(voice.id()).getRegistered();
+    }
+
+    public Voice create (Voice voice){
+        return voiceRepository.save(voice);
+    }
+
+}
